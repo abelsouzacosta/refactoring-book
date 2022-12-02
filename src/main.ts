4a3ef916801ce37a4199635c644b929d7bf781d8
Abel Junior <abel.costa@webjump.com.br>
2022-12-02 15:37:15 -0300

    refactor: outlining functions

--- src/main.ts
+++ src/main.ts
@@ -23,28 +23,28 @@ export let invoices = [
 
 export function statement(invoice) {
   return renderPlainText(createStatementData(invoice));
+}
 
-  function usd(amount) {
-    return new Intl.NumberFormat("en-US", {
-      style: "currency",
-      currency: "USD",
-      minimumFractionDigits: 2,
-    }).format(amount);
+function usd(amount) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format(amount);
+}
+
+function renderPlainText(data) {
+  let result = `Statement for ${data.customer}\n`;
+
+  for (let performance of data.performances) {
+    result += `${performance.play.name}: ${usd(performance.amount / 100)} (${
+      performance.audience
+    } seats)\n`;
   }
 
-  function renderPlainText(data) {
-    let result = `Statement for ${data.customer}\n`;
-
-    for (let performance of data.performances) {
-      result += `${performance.play.name}: ${usd(performance.amount / 100)} (${
-        performance.audience
-      } seats)\n`;
-    }
-
-    result += `Amount owed is ${usd(data.totalAmount / 100)}\n`;
-    result += `You earned ${data.totalVolumeCredits} credits\n`;
-    return result;
-  }
+  result += `Amount owed is ${usd(data.totalAmount / 100)}\n`;
+  result += `You earned ${data.totalVolumeCredits} credits\n`;
+  return result;
 }
 
 console.log(statement(invoices[0]));

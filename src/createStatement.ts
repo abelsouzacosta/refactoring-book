7696a93c464c95ebe9cedddff572ec0fc49bf8d0
junior <abelsouzacosta@gmail.com>
2022-12-04 16:12:06 -0300

    refactor: move function

--- src/createStatement.ts
+++ src/createStatement.ts
@@ -5,6 +5,40 @@ export let plays = {
   othello: { name: "Othello", type: "tragedy" },
 };
 
+export class PerformanceCalculator {
+  constructor(performance, play) {
+    Object.assign(this, { performance, play });
+  }
+
+  /**
+   * Calculates the amount charged for a performance
+   * TODO: replace conditional with polymorphism -> strategy | factory | chain of responsibility
+   * @param performance performance object
+   */
+  amount() {
+    let result = 0;
+    switch (this.play.type) {
+      case "tragedy":
+        result = 40000;
+        if (this.performance.audience > 30) {
+          result += 1000 * (this.performance.audience - 30);
+        }
+        break;
+      case "comedy":
+        result = 30000;
+        if (this.performance.audience > 20) {
+          result += 10000 + 500 * (this.performance.audience - 20);
+        }
+        result += 300 * this.performance.audience;
+        break;
+      default:
+        throw new Error(`unknown type: ${performance.play.type}`);
+    }
+
+    return result;
+  }
+}
+
 export function createStatementData(invoice) {
   let statementData = {};
 
@@ -14,9 +48,13 @@ export function createStatementData(invoice) {
   statementData.totalVolumeCredits = getTotalVolumeCredits(statementData);
 
   function enrichPerformance(performance) {
+    let calculator = new PerformanceCalculator(
+      performance,
+      playFrom(performance)
+    );
     let result = Object.assign({}, performance);
 
-    result.play = playFrom(result);
+    result.play = calculator.play;
     result.amount = amountFor(result);
     result.volumeCredits = volumeCreditsForPerformance(result);
 
@@ -30,41 +68,13 @@ function playFrom(performance) {
   return plays[performance.playID];
 }
 
-/**
- * Calculates the amount charged for a performance
- * TODO: replace conditional with polymorphism -> strategy | factory | chain of responsibility
- * @param performance performance object
- */
-function amountFor(performance) {
-  let result = 0;
-  switch (performance.play.type) {
-    case "tragedy":
-      result = 40000;
-      if (performance.audience > 30) {
-        result += 1000 * (performance.audience - 30);
-      }
-      break;
-    case "comedy":
-      result = 30000;
-      if (performance.audience > 20) {
-        result += 10000 + 500 * (performance.audience - 20);
-      }
-      result += 300 * performance.audience;
-      break;
-    default:
-      throw new Error(`unknown type: ${performance.play.type}`);
-  }
-
-  return result;
-}
-
 function volumeCreditsForPerformance(performance) {
   let result = 0;
 
-  result += Math.max(performance.audience - 30, 0);
+  result += Math.max(this.performance.audience - 30, 0);
 
   if ("comedy" === performance.play.type)
-    result += Math.floor(performance.audience / 5);
+    result += Math.floor(this.performance.audience / 5);
 
   return result;
 }
